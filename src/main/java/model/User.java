387317f0911d87ca8b8c88d22ad83356bea8b168
samuel <samuel.j.doria@gmail.com>
2017-06-22 20:00:49 -0600

    Updating user class to match user form on frontend.

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -5,10 +5,13 @@ package model;
  */
 public class User {
 
-    private String wNumber;
     private int id;
+    private String wNumber;
+    private String email;
     private String firstName;
     private String lastName;
+    private Address address;
+    private String password;
 
     public User(String wNumber, String firstName, String lastName) {
         this.wNumber = wNumber;
@@ -17,6 +20,30 @@ public class User {
     }
 
     public User() {}
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
     public int getId() {
     	return this.id;
