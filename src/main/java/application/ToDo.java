1c7d8722808bf89e12358291e670ead2218a61ae
samuel <samuel.j.doria@gmail.com>
2017-08-05 23:20:09 -0600

    Updated todo list

--- src/main/java/application/ToDo.java
+++ src/main/java/application/ToDo.java
@@ -6,14 +6,12 @@ package application;
  */
 public class ToDo {
 
-    // Todo: figure out the app user creation problem where the app can only create users using a current admin user
     // Todo: Add Listing Attachment Resource.
     // Todo: Add database repository.
     // Todo: Add required field validation around the resources
     // Todo: Remove instances of null in the representation layer.
-    // Todo: Change user salt when they change their password.
     // Todo: Standardize Error Messages
-    // Todo: Do some major clean up of exception handling, crappy code thrown together, etc... Basically once
-    // Todo: everything is working go back and do it again but better now that you have more time :D.
+    // Todo: Do some major clean up of exception handling, crappy code thrown together, etc... Basically once...
+    // Todo: ...everything is working go back and do it again but better now that you have more time :D.
     // Todo: Add resource specific filters/interceptors to clean up duplicated authorization code.
 }
