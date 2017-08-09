c280a26a3274eb29e4d4be2c543f8f10781026a1
samuel <samuel.j.doria@gmail.com>
2017-08-08 20:54:48 -0600

    Added user listings resource.

--- src/main/java/resource/UserListingResource.java
+++ src/main/java/resource/UserListingResource.java
@@ -1,24 +1,17 @@
 package resource;
 
 import annotation.AuthorizationRequired;
-import application.ApplicationConstants;
 import model.Listing;
-import model.User;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import repository.ListingRepository;
-import repository.UserRepository;
 import utility.ListingUtility;
-import utility.UserUtility;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
@@ -26,9 +19,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static application.ApplicationConstants.ID;
-import static application.ApplicationConstants.TOKEN_USER_ATTRIBUTE;
 import static application.ApplicationConstants.USER_LISTINGS_RESOURCE;
-import static application.ApplicationConstants.USER_NAME;
 
 @CrossOrigin(origins = "*")
 @RestController
